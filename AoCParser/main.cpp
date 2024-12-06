72cba06131fdea5b7f2d4dd5644f8432b66b4dbf
arget4me <sn.jn@telia.com>
2024-12-06 01:12:01 +0100

    Solve Day3b

--- AoCParser/main.cpp
+++ AoCParser/main.cpp
@@ -64,6 +64,7 @@ void RunAllTests()
 	RunTest("days/day2.aoc", testsFailed);
 	RunTest("days/day2b.aoc", testsFailed);
 	RunTest("days/day3.aoc", testsFailed);
+	RunTest("days/day3b.aoc", testsFailed);
 
 	if (testsFailed) {
 		PushConsoleColor(CONSOLE_COLOR::RED);
@@ -80,7 +81,7 @@ void RunAllTests()
 void RunTests() {
 	bool testsFailed = false;
 	std::cout << "RUNNING TESTS\n" << std::endl;
-	RunTest("days/day3.aoc", testsFailed);
+	RunTest("days/day3b.aoc", testsFailed);
 
 	if (testsFailed) {
 		PushConsoleColor(CONSOLE_COLOR::RED);
